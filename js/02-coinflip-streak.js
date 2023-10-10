b40ded60402ddc5cb3e35ae2ceea64f613c88584
KRUPSKAIA ANGULO <krupskaia@outlook.com>
2023-10-10 11:46:44 -0700

    middle

--- js/02-coinflip-streak.js
+++ js/02-coinflip-streak.js
@@ -1,11 +1,11 @@
 var coinFlip;
-var i;
-var amount = window.prompt("Plase enter a number between 1 to 10");
-for (i = 1; i <= amount; i += 1) {
+
+do {
     coinFlip = Math.round(Math.random());
+    window.console.log(coinFlip);
     if (coinFlip) {
         window.console.log("Tails");
     } else {
         window.console.log("Heads");
     }
-}
+} while (!coinFlip);
